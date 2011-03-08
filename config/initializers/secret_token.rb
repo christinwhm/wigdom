707cca91ce70892008992d87118df6b41a488396
christinwhm <christinwhm@gmail.com>
2011-03-08 19:44:48 +0800

    1. First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wigdom::Application.config.secret_token = '2044000bd1d876a1943536f5ce7fe2f68a50ef9513e868d86af6dc4ab8d38533070452b1e3de15b2ac647104a6d751bee271a053ea5a9a127fb67a885f3a3f1e'
