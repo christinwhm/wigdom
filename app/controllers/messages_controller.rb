3bdf85a212ab62bd6e68c11e3cddf4ba4e99b470
Hao Liu <leomayleomay@gmail.com>
2011-03-20 08:30:34 +0800

    1. Minor changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,8 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+
   def create
     if verify_recaptcha
       @message = Message.new(params[:message])
