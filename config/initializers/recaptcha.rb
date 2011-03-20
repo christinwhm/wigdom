35802b49070d6a691883cb8425b4f82b2f1475f3
Hao Liu <leomayleomay@gmail.com>
2011-03-20 18:23:23 +0800

    1. Modify the reCaptcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Ld01gEAAAAAAKE_qjviCrQ648OnG1GYEkkg4Q8a'
-  config.private_key = '6Ld01gEAAAAAAB6zSXDK6JHjeIS-gcSnYFB7zl8r'
+  config.public_key  = '6LeCqMISAAAAAKW5ix8pqcz_JAs9OsG7gejouKgg'
+  config.private_key = '6LeCqMISAAAAAAQhPTAeYjjMiYBDI3sR4nhrU14d'
 end
